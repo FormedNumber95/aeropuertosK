7718e1ac93cfc8357daa949d73366f00e1f2616d
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-24 13:54:15 +0200

    acabado relog
    acabado el relog para que funcione y hecho que al cerrar la ventana, el programa finalice
    24/10/2024

--- src/main/java/es/aketzagonzalez/aeropuertosK/MainApp.java
+++ src/main/java/es/aketzagonzalez/aeropuertosK/MainApp.java
@@ -8,10 +8,20 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
+/**
+ * Clase MainApp.
+ */
 public class MainApp extends Application {
+    
+    /** El stage. */
     private static Stage stage;
 
+    /**
+     * Da al stage y al titulo un valor y hace que no sea reescalable.
+     *
+     * @param s the s
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
     @Override
     public void start(@SuppressWarnings("exports") Stage s) throws IOException {
         stage=s;
@@ -19,10 +29,23 @@ public class MainApp extends Application {
         setRoot("relog","RELOG");
     }
 
+    /**
+     * setea el root lanzando el setRoot de 2 parametros.
+     *
+     * @param fxml the new root
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
     static void setRoot(String fxml) throws IOException {
         setRoot(fxml,stage.getTitle());
     }
 
+    /**
+     * fija la ruta y el titulo del stage.
+     *
+     * @param fxml the fxml
+     * @param title the title
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
     static void setRoot(String fxml, String title) throws IOException {
         Scene scene = new Scene(loadFXML(fxml));
         stage.setTitle(title);
@@ -30,14 +53,35 @@ public class MainApp extends Application {
         stage.show();
     }
 
+    /**
+     * carga el fxml.
+     *
+     * @param fxml the fxml
+     * @return the parent
+     * @throws IOException Signals that an I/O exception has occurred.
+     */
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("/fxml/"+fxml + ".fxml"));
         return fxmlLoader.load();
     }
 
 
+    /**
+     * Metodo que lanza la aplicacion.
+     *
+     * @param args the arguments
+     */
     public static void main(String[] args) {
         launch(args);
     }
+    
+    /**
+     * Getter del stage.
+     *
+     * @return the stage
+     */
+    public static Stage getStage() {
+		return stage;
+	}
 
 }
--- src/main/java/es/aketzagonzalez/aeropuertosK/relogController.java
+++ src/main/java/es/aketzagonzalez/aeropuertosK/relogController.java
@@ -1,26 +1,141 @@
 package es.aketzagonzalez.aeropuertosK;
 
-import javafx.fxml.FXML;
-import javafx.scene.image.ImageView;
+import java.time.LocalDateTime;
+import java.util.Timer;
+import java.util.TimerTask;
 
+import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.application.Platform;
+import javafx.stage.WindowEvent; 
+
+/**
+ * Clase relogController.
+ */
 public class relogController {
 
+    /** El id de la imagen de la hora de la derecha. */
     @FXML
     private ImageView idHDer;
 
+    /** El id de la imagen de la hora de la izquierda. */
     @FXML
     private ImageView idHIz;
 
+    /** El id de la imagen del minuto de la derecha */
     @FXML
     private ImageView idMinDer;
 
+    /** El id de la imagen del minuto de la izquierda. */
     @FXML
     private ImageView idMinIz;
 
+    /** El id de la imagen del segundo de la derecha. */
     @FXML
     private ImageView idSegDer;
 
+    /** TEl id de la imagen del segundo de la izquierda */
     @FXML
     private ImageView idSegIz;
+    
+    /** El timer. */
+    private Timer timer;
+    
+    /**
+     * Inicializa el relog y finaliza la ejecucion del programa al cerrar la ventana.
+     */
+    @FXML
+    public void initialize() {
+    	MainApp.getStage().setOnCloseRequest((WindowEvent event) -> {
+    		parar();
+            Platform.exit();
+        });
+    	iniciarRelog();
+    }
+
+	/**
+	 * Ejecuta el metodo actualizar relog y dice que debe cambiar cada 1000 ms (1s).
+	 */
+	private void iniciarRelog() {
+		timer=new Timer();
+		timer.scheduleAtFixedRate(new TimerTask() {
+            @Override
+            public void run() {
+            	actualizarRelog();
+            }
+        }, 0, 1000);
+	}
+	
+	/**
+	 * Update clock.
+	 */
+	private void actualizarRelog() {
+		Platform.runLater(() -> {
+			LocalDateTime currentTime = LocalDateTime.now();
+			int hours = currentTime.getHour();
+	        int minutes = currentTime.getMinute();
+	        int seconds = currentTime.getSecond();
+	        actualizarImagen(idHIz, hours / 10);
+	        actualizarImagen(idHDer, hours % 10);
+	        actualizarImagen(idMinIz, minutes / 10);
+	        actualizarImagen(idMinDer, minutes % 10);
+	        actualizarImagen(idSegIz, seconds / 10);
+	        actualizarImagen(idSegDer, seconds % 10);
+		});
+	}
+
+	/**
+	 * Actualiza las imagenes para que se muestre la hora actual.
+	 *
+	 * @param imagen La imagen
+	 * @param i the i
+	 */
+	private void actualizarImagen(ImageView imagen, int i) {
+		String num="";
+		switch (i) {
+		case 0:
+			num="ZERO";
+			break;
+		case 1:
+			num="ONE";
+			break;
+		case 2:
+			num="TWO";
+			break;
+		case 3:
+			num="THREE";
+			break;
+		case 4:
+			num="FOUR";
+			break;
+		case 5:
+			num="FIVE";
+			break;
+		case 6:
+			num="SIX";
+			
+			break;
+		case 7:
+			num="SEVEN";
+			break;
+		case 8:
+			num="EIGHT";
+			break;
+		case 9:
+			num="NINE";
+			break;
+		}
+		imagen.setImage(new Image(getClass().getResource("/imagenes/"+num+".png").toString()));
+	}
+	
+	/**
+	 * para el relog, haciendo que la ejecucion del programa finalice.
+	 */
+	private void parar() {
+        if (timer != null) {
+            timer.cancel();
+        }
+    }
 
 }
