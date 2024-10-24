8aa328d01ac2642c6a973af75cfb45960c26a9d0
FormedNumber95 <152279572+FormedNumber95@users.noreply.github.com>
2024-10-24 13:14:45 +0200

    Initial commit

---
+++ src/main/java/es/aketzagonzalez/aeropuertosK/relogController.java
@@ -0,0 +1,26 @@
+package es.aketzagonzalez.aeropuertosK;
+
+import javafx.fxml.FXML;
+import javafx.scene.image.ImageView;
+
+public class relogController {
+
+    @FXML
+    private ImageView idHDer;
+
+    @FXML
+    private ImageView idHIz;
+
+    @FXML
+    private ImageView idMinDer;
+
+    @FXML
+    private ImageView idMinIz;
+
+    @FXML
+    private ImageView idSegDer;
+
+    @FXML
+    private ImageView idSegIz;
+
+}
